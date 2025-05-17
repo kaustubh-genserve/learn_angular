d733a19dbd0612846b72f15d9e313fb33e10be58
kaustubh-genserve <kaustabh@genserve.ai>
2025-05-18 02:54:06 +0530

    feat: add user component with template and styles

--- first-angular-app/NOTES.md
+++ first-angular-app/NOTES.md
@@ -1,1 +1,10 @@
 ![alt text](image.png)
+
+# Making new components using NG CLI
+
+`ng generate component <component-name> [options]`
+
+OR 
+
+
+`ng g c koko-component`
--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 //  Below is a TS Angular Decorater which adds ,etadata
 //  selector is the name of the component in the index.html file
@@ -10,9 +11,9 @@ import { HeaderComponent } from "./header/header.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
