c2600bd4784b2e44f58ece9f642417b598b5dd9f
kaustubh-genserve <kaustabh@genserve.ai>
2025-05-19 15:26:41 +0530

    feat: enhance user component with required input properties and output event for user selection

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,8 +1,17 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  Input,
+  signal,
+  input,
+  Output,
+  EventEmitter,
+  output,
+} from '@angular/core';
 // import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 // Math.random() returns a random number between 0 and 1
 // Math.floor() rounds down to the nearest integer
 // DUMMY_USERS.length returns the number of elements in the array
@@ -15,17 +24,55 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
+
+// We can add required propertu in the input parameter to make sire that we ALWAYS have the value
+// We can use the ! operator to tell TypeScript that we are sure that the value will be set somewhere in the code
+// Input is going to have the value of the property that we are going to pass from the parent component
+//  In our case, we have exported dumm users in app.component.ts
+// Input() is going to go to app.ts and get the value of the property
+
+// We set Output devorator with the EventEmitter import from angular core when we want to output the valuve of some reaction to the browser
+//  In our case, we are going to output the id of the user that we have selected
 export class UserComponent {
-  @Input() avatar!: string;
-  // ! measn we know that value will be set somewhere from angular
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    // we created a select output property
+    // and we are going to emit the id of the user that we have selected
+    // This is going to be used in the app.component.ts file
+    this.select.emit(this.id);
+  }
 }
+
+// SIGNAL INPUT ( Using input wil small i
+// input is a generic TS type that is used to create a signal
+//  It is going to accept data from app.ts and use logic in the UserComponent
+//  Imagepath is a computed property that is going to return the path to the image
+// input is a read-only property. We cab't chnage the value from inside the cpmponents.
+
+//  WHEN TO USE INPUT and input ? :: It's a new angluar VS old angular thing
+// export class UserComponent {
+//   avatar = input.required<string>();
+//   name = input.required<string>();
+//   id = input.required<string>();
+
+//   // Output will create a new Eventemmiter object internally so we don't have to do it manually
+//   select = output<string>();
+
+//   imagePath = computed(() => 'assets/users/' + this.avatar);
+
+//   onSelectUser() {
+//     this.select.emit(this.id());
+//   }
+// }
 
 // export class UserComponent {
 // selectedUser = signal(DUMMY_USERS[randomIndex]);
