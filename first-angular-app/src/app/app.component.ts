4c1e185a51014e6fd37edbb390e8261862cab3f8
kaustubh-genserve <kaustabh@genserve.ai>
2025-05-30 13:16:34 -0400

    Add investment calculation logic and style user input form
    - Implemented a function to calculate investment results based on initial investment, annual investment, expected return, and duration.
    - Created a new CSS file to style the user input form with a modern design, including responsive layout and hover effects for buttons.

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,34 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
-import { TaskComponent } from './task/task.component';
 
-//  Below is a TS Angular Decorater which adds ,etadata
-//  selector is the name of the component in the index.html file
-//  templateUrl is the path to the HTML file of the component
-//  styleUrl is the path to the CSS file of the component
-//  The component is the root component of the applicatio
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  users = DUMMY_USERS;
-
-  selectedUserId?: string;
-
-  get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId)!;
-  }
-  //  We used get like this because we want to use the selectedUser property in the HTML file
-  //  onSelectUser is a main method and get selected user is a getter PROPERTY.
-  onSelectUser(id: string) {
-    this.selectedUserId = id;
-    console.log('Selected user ID:', this.selectedUserId);
-  }
-}
+export class AppComponent {}
