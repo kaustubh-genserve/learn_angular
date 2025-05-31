3f39b1e01280c748670aeaca2dcab33b2e91f1de
kaustubh-genserve <kaustabh@genserve.ai>
2025-05-31 00:18:40 -0400

    refactor: reorganize component styles and remove unused CSS files

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvesmentresultsComponent } from './invesmentresults/invesmentresults.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserInputComponent, InvesmentresultsComponent],
 })
 export class AppComponent {}
